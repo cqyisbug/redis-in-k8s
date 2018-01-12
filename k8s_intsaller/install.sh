7087ab886a75e7fe9d30c39f248a98ee4a08f5eb
caiqiyuan (RD) <cai.qiyuan@h3c.com>
2018-01-12 16:17:34 +0800

    fix a bug in sed

--- k8s_intsaller/install.sh
+++ k8s_intsaller/install.sh
@@ -10,7 +10,7 @@ yum install -y docker flannel etcd kubernetes *rhsm*
 
 echo "Initing etcd...."
 cp ./etcd.conf ./etcd.conft
-sed -i "s/%master%/$me/" ./etcd.conft
+sed -i "s/%master%/$me/g" ./etcd.conft
 mv /etc/etcd/etcd.conf /etc/etcd/etcd.conf.bak
 mv ./etcd.conft /etc/etcd/etcd.conf
 
@@ -26,8 +26,8 @@ etcdctl mk /coreos.com/network/config '{"Network":"172.17.0.0/16"}'
 echo "Initing k8s..."
 cp ./apiserver ./apiservert
 cp ./config ./configt
-sed -i "s/%master%/$me/" ./apiservert
-sed -i "s/%master%/$me/" ./configt
+sed -i "s/%master%/$me/g" ./apiservert
+sed -i "s/%master%/$me/g" ./configt
 
 mv /etc/kubernetes/apiserver /etc/kubernetes/apiserver.bak
 mv ./apiservert /etc/kubernetes/apiserver
@@ -37,7 +37,7 @@ mv ./configt /etc/kubernetes/config
 echo "Initing flannel & docker ..."
 
 cp ./flanneld ./flanneldt
-set -i "s/%master%/$me/" ./flanneldt
+set -i "s|%master%|$me|g" ./flanneldt
 mv /etc/sysconfig/flanneld /etc/sysconfig/flanneld.bak
 mv ./flanneldt /etc/sysconfig/flanneld
 
@@ -47,8 +47,9 @@ echo "Starting combining ..."
 systemctl daemon-reload
 
 for s in etcd kube-apiserver kube-scheduler kube-controller-manager kubelet kube-proxy flanneld docker ; do
+		echo "**********       $s       ************"
         systemctl restart $s
         systemctl enable $s
-        systemctl status $s | grep Active
+        systemctl status $s 
 done
 
