20c8ba9ba7be6adef99f0a85f363c518a04859d7
marscqy <cqyisbug@163.com>
2018-01-12 11:22:27 +0800

    add k8s remover

---
+++ k8s_intsaller/uninstall.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+services=(etcd kube-apiserver kube-scheduler kube-controller-manager kubelet kube-proxy flanneld docker)
+
+echo "Stoping Services..."
+
+for svc in $services ; do 
+	systemctl stop $svc
+	systemctl disable $svc
+done
+
+echo "Removing Softwares..."
+
+rpm -qa | grep -E "etcd|docker|flannel|kubernetes"
+
+echo "Removing docker0 Bridge ..."
+
+ip link set dev docker0 down
+# brctl delbr docker0
+
+echo "Removing files..."
+find / -name "*.rpmsave" | xargs rm -rf 
+
+rm -rf /run/flannel
+rm -rf /run/docker
+rm -rf /var/lib/docker
+rm -rf /data/docker
+rm -rf /etc/docker
+rm -rf /var/lib/etcd
+rm -rf /etc/etcd
+rm -rf /run/kubernetes
+rm -rf /etc/kubernetes
