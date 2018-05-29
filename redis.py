891ae91d161d434bec8cee343326f15c2f298e62
cqyisbug <cqyisbug@163.com>
2018-05-29 17:25:07 +0800

    [M] fix spelling

--- redis.py
+++ redis.py
@@ -126,7 +126,7 @@ def check_config(config):
             return False
 
         # hostnetowrk
-        if str(config["hostnetowrk"]).lower == "true":
+        if str(config["hostnetwork"]).lower == "true":
             if exists_resource("node","Ready",bool_result=False) < int(config["redis_replicas"]) :
                 print("in hostnetowrk mode,make sure nodes >= redis_replicas")
                 return False
